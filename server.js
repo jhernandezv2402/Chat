b5058bf002bfb78abf3a1c92dda70b391660fd86
Jesus <jhernandezv2402@outlook.com>
2015-05-26 13:13:27 -0500

    db online

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mensajes = require('./server/models/msg')();
 var routes = require('./server/routes')(express);
 // var port = process.env.PORT || 1850;
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/app/www'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
